cd7105a0bb6d3b8c812bc826eac82ce10d0fc85a
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-03-08 21:07:42 +0530

    Worked on Login

--- src/main/java/me/mtron/user/Login.java
+++ src/main/java/me/mtron/user/Login.java
@@ -1,4 +1,48 @@
 package me.mtron.user;
 
-public class Login {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JDialog{
+
+    private JLabel imageLabel;
+    private JTextField tfEmail;
+    private JPasswordField pwdField;
+    private JButton clearButton;
+    private JButton loginButton;
+    private JPanel loginPanel;
+
+    public Login(JFrame parent) {
+        super(parent);
+        setTitle("Login");
+        setContentPane(loginPanel);
+        setMinimumSize(new Dimension(700, 480));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setResizable(false);
+
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Login");
+        Login login = new Login(null);
+    }
 }
