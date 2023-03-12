796e39a4fc7600558ba7f4a0b9396292ab8a70e1
INFINITY <nirajnimantha@gmail.com>
2023-03-12 13:25:02 +0530

    dashboard init

---
+++ src/main/java/me/mtron/admin/Dashboard.java
@@ -0,0 +1,13 @@
+package me.mtron.admin;
+
+import javax.swing.*;
+
+public class Dashboard extends JFrame {
+
+    private JPanel dashboardPanel;
+    private JLabel welcomeLable;
+    private JButton createChatButton;
+    private JButton removeUserButton;
+    private JButton unsubscribeUserButton;
+    private JButton subscribeUserButton;
+}
