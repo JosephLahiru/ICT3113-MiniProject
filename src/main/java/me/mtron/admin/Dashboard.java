ac8fcea02b88cc1e7c9663fa85e1ad6d10fb94be
INFINITY <nirajnimantha@gmail.com>
2023-03-18 22:46:31 +0530

    sync

--- src/main/java/me/mtron/admin/Dashboard.java
+++ src/main/java/me/mtron/admin/Dashboard.java
@@ -9,4 +9,16 @@ public class Dashboard extends JFrame {
     private JButton subscribeUsersButton;
     private JButton unsubscribeUsersButton;
     private JButton removeUsersButton;
+
+    public Dashboard() {
+        super("Dashboard");
+        this.setContentPane(dashboardPanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Dashboard();
+    }
 }
