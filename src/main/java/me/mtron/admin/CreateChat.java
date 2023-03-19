8b1636b6dc73d0a9f5600c67295f42e538f9e51f
INFINITY <nirajnimantha@gmail.com>
2023-03-19 07:00:52 +0530

    create-chat-gui

--- src/main/java/me/mtron/admin/CreateChat.java
+++ src/main/java/me/mtron/admin/CreateChat.java
@@ -1,4 +1,26 @@
 package me.mtron.admin;
 
-public class CreateChat {
+import javax.swing.*;
+
+public class CreateChat extends JFrame {
+    private JPanel createChatPanel;
+    private JTable viewChatTable;
+    private JTextField textFieldChatName;
+    private JButton addButton;
+    private JButton resetButton;
+    private JTextField textFieldChatDiscription;
+
+    public CreateChat() {
+        super("Create Chat");
+        this.setContentPane(createChatPanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new CreateChat();
+    }
 }
--- src/main/java/me/mtron/admin/Dashboard.java
+++ src/main/java/me/mtron/admin/Dashboard.java
@@ -1,6 +1,8 @@
 package me.mtron.admin;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Dashboard extends JFrame {
 
@@ -14,8 +16,16 @@ public class Dashboard extends JFrame {
         super("Dashboard");
         this.setContentPane(dashboardPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
         this.pack();
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
+        createChatButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new CreateChat();
+            }
+        });
     }
 
     public static void main(String[] args) {
