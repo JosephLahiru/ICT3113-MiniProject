4f2a95a115cae188123f2a42f4b5bfb2f04c4b84
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-03-20 19:51:56 +0530

    Created User Chat Area

---
+++ src/main/java/me/mtron/user/ChatArea.java
@@ -0,0 +1,27 @@
+package me.mtron.user;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChatArea extends JFrame{
+    private JButton cHomeButton;
+    private JPanel ChatArea;
+
+    public ChatArea() {
+        super("ChatArea");
+        this.setContentPane(ChatArea);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        cHomeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ChatArea.this.dispose();
+                new Dashboard();
+            }
+        });
+    }
+}
